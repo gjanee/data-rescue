a22c03351fcb54614c16edd32df9d92294383175
Greg Janée <gjanee@ucsb.edu>
2025-09-25 16:13:04 -0700

    Add getter scripts

---
+++ get_object.R
@@ -0,0 +1,27 @@
+# Gets a science metadata object (or any other kind of object, for
+# that matter) from KNB and writes it to standard output.
+#
+# Usage: Rscript get_object.R PROD|SANDBOX pid
+#
+# Authentication isn't required for public objects, but for private
+# objects this script assumes the authentication token
+# (`dataone_token` or `dataone_test_token`) is set in .Rprofile.
+
+library(dataone)
+
+nodes <- list(
+  PROD="urn:node:KNB",
+  SANDBOX="urn:node:mnSandboxUCSB1"
+)
+
+args <- commandArgs(trailingOnly=TRUE)
+if (length(args) != 2 || !(args[1] %in% c("PROD", "SANDBOX"))) {
+  stop("Usage: Rscript get_object.R PROD|SANDBOX pid")
+}
+environment <- args[1]
+pid <- args[2]
+
+cn <- CNode(environment)
+mn <- getMNode(cn, nodes[[environment]])
+
+cat(rawToChar(getObject(mn, pid)))
